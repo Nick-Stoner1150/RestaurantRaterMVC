def48b7096cfa3c048e0971ea50c1daa7d1adde9
Nicholas Stoner <nicholas.c.stoner@gmail.com>
2021-10-09 11:18:17 -0400

    Add project files.

---
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -0,0 +1,43 @@
+﻿using RestaurantRaterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRaterMVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private readonly RestaurantDbContext _db;
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Restaurant/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(restaurant);
+        }
+    }
+}
